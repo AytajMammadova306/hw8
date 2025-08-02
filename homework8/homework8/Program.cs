8f514a9b89c9c3f6c4a34f41f06f5998bd8dff83
AytajMammadova306 <aytajamam@code.edu.az>
2025-08-02 19:33:41 +0400

    initial

---
+++ homework8/homework8/Program.cs
@@ -0,0 +1,20 @@
+﻿using homework8.models;
+namespace homework8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student1 = new Student("Aytaj", "Mammadova", "PB306", 99/* umidler*/);
+            //student1.PrintInfo();
+            //student1.GetDiplomDegree();
+
+            //Product product1 = new Product("ASUS","ViviBook",1500,200,2);
+            //product1.PrintInfo();
+            //product1.Sale(2);
+            //product1.PrintInfo();
+            //product1.Refill(4);
+            //product1.PrintInfo();
+        }
+    }
+}
---
+++ homework8/homework8/models/Product.cs
@@ -0,0 +1,54 @@
+﻿
+namespace homework8.models
+{
+    internal class Product
+    {
+        public string BrandName;
+        public string Model;
+        public int Price;
+        public int Cost;
+        public int Income;
+        public int Count;
+
+        public Product(string brandName, string model, int price,int cost,int count)
+        {
+            BrandName = brandName;
+            Model = model;
+            Price = price;
+            Cost = cost;
+            if (count > 0)
+            {
+                Count = count;
+            }
+            else
+            {
+                count = 0;
+            }
+            
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Brand Name:{BrandName}, Model:{Model}, Price:{Price}, Cost:{Cost}, Count:{Count}, Income:{Income}");
+        }
+
+        public void Sale(int soldcount)
+        {
+            if (soldcount <= Count)
+            {
+                Count -= soldcount;
+                Income += soldcount * Price - Cost;
+            }
+            else
+            {
+                Console.WriteLine("you don't have that much products");
+            }
+        }
+
+        public int Refill(int count)
+        {
+            Count += count;
+            return Count;
+        }
+
+    }
+}
